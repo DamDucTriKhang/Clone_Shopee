37a9216b546dc9fb36dc64024abf54e0aa2ecbab
DamDucTriKhang <khangddt@rubyonring.com>
2022-07-12 16:48:54 +0700

    install_devise

--- db/migrate/20220712025647_create_line_items.rb
+++ db/migrate/20220712025647_create_line_items.rb
@@ -1,6 +1,7 @@
 class CreateLineItems < ActiveRecord::Migration[6.1]
   def change
     create_table :line_items do |t|
+      t.references :product, foreign_key: true, null: false
       t.integer :price
       t.integer :quantity
       t.integer :itemable_id
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_12_025647) do
+ActiveRecord::Schema.define(version: 2022_07_12_083325) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "contact_name"
     t.string "phone"
     t.string "address"
@@ -31,12 +34,14 @@ ActiveRecord::Schema.define(version: 2022_07_12_025647) do
   end
 
   create_table "line_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
     t.integer "price"
     t.integer "quantity"
     t.integer "itemable_id"
     t.string "itemable_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
   create_table "logistics", force: :cascade do |t|
@@ -48,9 +53,9 @@ ActiveRecord::Schema.define(version: 2022_07_12_025647) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "contact_id", null: false
-    t.integer "logistic_id", null: false
-    t.integer "voucher_id", null: false
+    t.bigint "contact_id", null: false
+    t.bigint "logistic_id", null: false
+    t.bigint "voucher_id", null: false
     t.integer "total_price"
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
@@ -61,17 +66,17 @@ ActiveRecord::Schema.define(version: 2022_07_12_025647) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.integer "product_id", null: false
+    t.bigint "shop_id", null: false
     t.string "name"
     t.text "description"
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["product_id"], name: "index_products_on_product_id"
+    t.index ["shop_id"], name: "index_products_on_shop_id"
   end
 
   create_table "purchases", force: :cascade do |t|
-    t.integer "stock_id", null: false
+    t.bigint "stock_id", null: false
     t.string "quatity"
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
@@ -89,7 +94,7 @@ ActiveRecord::Schema.define(version: 2022_07_12_025647) do
   end
 
   create_table "stocks", force: :cascade do |t|
-    t.integer "product_id", null: false
+    t.bigint "product_id", null: false
     t.string "quatity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -97,8 +102,8 @@ ActiveRecord::Schema.define(version: 2022_07_12_025647) do
   end
 
   create_table "user_vouchers", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "voucher_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "voucher_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_user_vouchers_on_user_id"
@@ -107,29 +112,39 @@ ActiveRecord::Schema.define(version: 2022_07_12_025647) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vouchers", force: :cascade do |t|
+    t.bigint "shop_id", null: false
     t.string "name"
     t.text "description"
     t.integer "discount_amount"
     t.integer "discount_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["shop_id"], name: "index_vouchers_on_shop_id"
   end
 
   add_foreign_key "carts", "users"
   add_foreign_key "contacts", "users"
+  add_foreign_key "line_items", "products"
   add_foreign_key "orders", "contacts"
   add_foreign_key "orders", "logistics"
   add_foreign_key "orders", "vouchers"
-  add_foreign_key "products", "products"
+  add_foreign_key "products", "shops"
   add_foreign_key "purchases", "stocks"
   add_foreign_key "stocks", "products"
   add_foreign_key "user_vouchers", "users"
   add_foreign_key "user_vouchers", "vouchers"
+  add_foreign_key "vouchers", "shops"
 end
