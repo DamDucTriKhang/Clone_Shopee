d56056e2ad5bd3cb39b0262bd2f6e16e632dd7be
DamDucTriKhang <khangddt@rubyonring.com>
2022-07-19 11:05:21 +0700

    edit_user_page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,28 @@ class UsersController < ApplicationController
 	def show
 	end
 
+	def edit
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      flash[:danger] = "You do not have access"
+    end
+	end
+
+  def update
+    if current_user.update(user_params)
+      flash[:success] = "User updated!"
+      sign_in(current_user, :bypass => true)
+      redirect_to user_path(current_user)
+    else
+      flash[:danger] = "User no update!"
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
