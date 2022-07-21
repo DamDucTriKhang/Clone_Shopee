d35017174d12eafaf7e6bb5e165ab2f20e4e3ee9
RoRHoangVietHung <hunghv@rubyonring.com>
2022-07-21 16:16:45 +0700

    index_contacts

--- db/migrate/20220711100111_create_users.rb
+++ db/migrate/20220711100111_create_users.rb
@@ -34,7 +34,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
 
 
       # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+      t.timestamps
     end
 
     add_index :users, :email,                unique: true
